a65d8e96927ce545e759aeb54346694a20ee02f9
DerrellJ81 <secretvaultproductions@gmail.com>
2025-04-18 21:46:24 -0700

    Update signalingServer.js

--- signalingServer.js
+++ signalingServer.js
@@ -17,19 +17,25 @@ wss.on("connection", (ws) => {
 
     if (type === "create") {
       sessions[code] = { host: ws, guest: null };
-    } else if (type === "join" && sessions[code]) {
-      sessions[code].guest = ws;
-      sessions[code].host.send(JSON.stringify({ type: "guest-joined" }));
+    } else if (type === "join") {
+      if (!sessions[code]) {
+        sessions[code] = { host: null, guest: ws };
+      } else {
+        sessions[code].guest = ws;
+        if (sessions[code].host) {
+          sessions[code].host.send(JSON.stringify({ type: "guest-joined" }));
+        }
+      }
     } else if (type === "signal") {
-      const target = sessions[code]?.host === ws
-        ? sessions[code].guest
-        : sessions[code].host;
-      if (target) {
+      const session = sessions[code];
+      if (!session) return;
+      const target = session.host === ws ? session.guest : session.host;
+      if (target && target.readyState === WebSocket.OPEN) {
         target.send(JSON.stringify({ type: "signal", payload }));
       }
     } else if (type === "input" && sessions[code]) {
       const host = sessions[code].host;
-      if (host) {
+      if (host && host.readyState === WebSocket.OPEN) {
         host.send(JSON.stringify({ type: "input", payload }));
       }
     }
